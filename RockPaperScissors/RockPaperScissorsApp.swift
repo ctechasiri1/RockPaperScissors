015cbd1e1822bb2d8b742025dfe161e2eec0734c
Chiraphat <ctechasiri@berkeley.edu>
2024-10-02 18:08:39 -0700

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Chiraphat Techasiri on 10/2/24.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
